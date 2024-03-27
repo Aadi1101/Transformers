b6a7ecaf253d8acd6942a2b53ce24a3fe0e31dc8
Aadi1101 <aadityakomerwar@gmail.com>
2024-03-28 01:59:25 +0530

    modified Encoder Layer

--- src/components/EncoderLayer.py
+++ src/components/EncoderLayer.py
@@ -10,10 +10,14 @@ class EncoderLayer(nn.Module):
     try:
         def __init__(self, d_model, num_heads, d_ff, dropout):
             super(EncoderLayer, self).__init__()
+            logging.info("In Encoding started with Multi Head Attention")
             self.self_attn = MultiHeadAttention(d_model, num_heads)
+            logging.info("In Encoding started with Position Wise Feed Forward")
             self.feed_forward = PositionWiseFeedForward(d_model, d_ff)
+            logging.info("Added 2 Normalization Layer.")
             self.norm1 = nn.LayerNorm(d_model)
             self.norm2 = nn.LayerNorm(d_model)
+            logging.info("Added Dropout in Encoding")
             self.dropout = nn.Dropout(dropout)
             
         def forward(self, x, mask):
